0191421f58e0952870ad0574eb5bdaf49fdf8382
shay-ff <tomohmmdali@gmail.com>
2026-06-13 02:15:23 +0530

    update : gluttony

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/nuxt/runtime";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
